64725ed96f30857f8515fa241c24b84fbc787d2b
justinlee05 <justinlee05@naver.com>
2021-06-08 08:22:29 +0900

    기능 수정

--- C.project/그린마일리지 관리 프로그램(1학년 1학기 프로그래밍 수행평가).c
+++ C.project/그린마일리지 관리 프로그램(1학년 1학기 프로그래밍 수행평가).c
@@ -1,23 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-
+#include <Windows.h>
 struct GM {
 	char name[10];
 	int num;
 	int score;
 }manage[21];
-
 char tempname[10];
-int i = 1,temp1, tempnum;
-
+int i = 1, temp1, tempnum;
 void Wrong() {
 	system("cls");
 	getchar();
 	printf("잘못된 입력입니다.");
-	
 }
-
 int Begin() {
 	int temp;
 	printf("[그린마일리지 관리 프로그램]\n수행하실 작업을 선택해주세요.\n1.등록 2.수정 3.검색 4.출력 5.종료\n입력:");
@@ -25,12 +21,10 @@ int Begin() {
 	system("cls");
 	return temp;
 }
-
-void Input(struct GM *p) {
+void Input(struct GM* p) {
 	while (1) {
 		printf("정보를 등록하시겠습니까?\n(1 입력시 확인, 0 입력시 취소)\n입력:");
 		scanf("%d", &tempnum);
-		
 		switch (tempnum) {
 		case 1: {
 			system("cls");
@@ -61,132 +55,142 @@ void Input(struct GM *p) {
 		return;
 	}
 }
-
 void Fix() {
 	while (1) {
-		printf("정보를 수정하시겠습니까?\n(1 입력시 수정, 0 입력 시취소)\n입력:");
+		if (i != 1) {}
+		else
+		{
+			system("cls");
+			printf("수정 가능한 정보가 없습니다.\n");
+			return;
+		}
+		printf("정보를 수정하시겠습니까?\n(1 입력시 수정, 0 입력시 취소)\n입력 :");
 		scanf("%d", &temp1);
 		switch (temp1) {
-			case 1: {
+		case 1: {
 				system("cls");
-				printf("수정할 정보의 번호를 입력해주세요.\n입력:");
+			while (1) {
+				printf("수정할 정보의 번호를 입력해주세요.");
+				printf("(수정 가능한 정보의 번호 : %d ~ %d)\n입력 :", 1, i - 1);
 				scanf("%d", &tempnum);
-				system("cls");
-				while (1)
-				{
-					
-					
-					printf("수정할 정보를 선택해주세요.\n1.이름 2.번호 3.점수 4.취소\n입력:");
-					scanf("%d", &temp1);
-					system("cls");
-					switch (temp1) {
-					case 1: {
-						printf("수정할 내용(이름)을 입력하세요.\n입력:");
-						scanf("%s", tempname);
-						strcpy(manage[tempnum].name, tempname);
-						system("cls");
-						printf("성공적으로 수정되었습니다!");
-						break;
-					}
-					case 2: {
-						printf("수정할 내용(번호)을 입력하세요.\n입력:");
-						scanf("%d", &temp1);
-						manage[tempnum].num = temp1;
-						system("cls");
-						printf("성공적으로 수정되었습니다!");
-						break;
-					}
-					case 3: {
-						printf("수정할 내용(점수)을 입력하세요.\n입력:");
-						scanf("%d", &temp1);
-						manage[tempnum].score = temp1;
-						system("cls");
-						printf("성공적으로 수정되었습니다!");
-						break;
-					}
-					case 4: {
-						return;
-					}
-					default:
-						Wrong();
-						continue;
-					}
+				if (tempnum >= 1 && tempnum <= 20)
 					break;
+				else
+					system("cls");
+					printf("잘못된 입력입니다.");
+			}
+				system("cls");
+			while (1)
+			{
+				printf("수정할 정보를 선택해주세요.\n1.이름 2.번호 3.점수 4.취소\n입력:");
+				scanf("%d", &temp1);
+				system("cls");
+				switch (temp1) {
+				case 1: {
+					printf("수정할 내용(이름)을 입력하세요.\n입력:");
+					scanf("%s", tempname);
+					strcpy(manage[tempnum].name, tempname);
+					system("cls");
+					printf("성공적으로 수정되었습니다!");
+					break;
+				}
+				case 2: {
+					printf("수정할 내용(번호)을 입력하세요.\n입력:");
+					scanf("%d", &temp1);
+					manage[tempnum].num = temp1;
+					system("cls");
+					printf("성공적으로 수정되었습니다!");
+					break;
+				}
+				case 3: {
+					printf("수정할 내용(점수)을 입력하세요.\n입력:");
+					scanf("%d", &temp1);
+					manage[tempnum].score = temp1;
+					system("cls");
+					printf("성공적으로 수정되었습니다!");
+					break;
+				}
+				case 4: {
+					return;
+				}
+				default:
+					Wrong();
+					continue;
 				}
 				break;
 			}
-			case 0: {
-				system("cls");
-				return;
-			}
-			default: {
-				Wrong();
-				continue;
-			}
+			break;
+		}
+		case 0: {
+			system("cls");
+			return;
+		}
+		default: {
+			Wrong();
+			continue;
+		}
 		}
 		Fix();
 		return;
 	}
 }
-
 void Search() {
-	
-	while (1){
+	while (1) {
 		printf("검색할 정보를 선택하세요.\n1.이름 2.번호 3.점수 4.종료\n입력:");
 		scanf("%d", &temp1);
 		system("cls");
 		switch (temp1) {
-			case 1: {
-				printf("검색할 이름을 입력하세요.\n입력:");
-				scanf("%s", tempname);
-				system("cls");
-				printf("검색결과와 유사한 정보는 다음과 같습니다.\n");
-				for (int i = 1; i <= 20; i++) {
-					
-					if(manage[i].name[0]==tempname[0] || manage[i].name[1] == tempname[1] || manage[i].name[2] == tempname[2])
-						printf("%d번 정보, \n", i);
-				}
-				break;
+		case 1: {
+			printf("검색할 이름을 입력하세요.\n입력:");
+			scanf("%s", tempname);
+			system("cls");
+			printf("검색결과와 유사한 정보는 다음과 같습니다.\n");
+			for (int i = 1; i <= 20; i++) {
+				if ((manage[i].name[0] == tempname[0] &&
+					manage[i].name[0] != NULL) || (manage[i].name[1] == tempname[1] && manage[i].name[1] != NULL) ||
+					(manage[i].name[2] == tempname[2] && manage[i].name[2] != NULL))
+					PrintResult(i);
 			}
-			case 2: {
-				printf("검색할 번호를 입력하세요.\n입력:");
-				scanf("%d", &temp1);
-				system("cls");
-				printf("검색결과와 일치하는 정보는 다음과 같습니다.\n");
-				for (int i = 1; i <= 20; i++) {
-					
-					if (manage[i].num == temp1)
-						printf("%d번 정보, ", i);
-				}
-				break;
+			break;
+		}
+		case 2: {
+			printf("검색할 번호를 입력하세요.\n입력:");
+			scanf("%d", &temp1);
+			system("cls");
+			printf("검색결과와 일치하는 정보는 다음과 같습니다.\n");
+			for (int i = 1; i <= 20; i++) {
+				if (manage[i].num == temp1)
+					PrintResult(i);
 			}
-			case 3: {
-				printf("검색할 점수를 입력하세요.\n입력:");
-				scanf("%d", &temp1);
-				system("cls");
-				printf("검색결과와 일치하는 정보는 다음과 같습니다.\n");
-				for (int i = 1; i <= 20; i++) {
-					
-					if (manage[i].num == temp1)
-						printf("%d번 정보, ", i);
-				}
-				break;
+			break;
+		}
+		case 3: {
+			printf("검색할 점수를 입력하세요.\n입력:");
+			scanf("%d", &temp1);
+			system("cls");
+			printf("검색결과와 일치하는 정보는 다음과 같습니다.\n");
+			for (int i = 1; i <= 20; i++) {
+				if (manage[i].num == temp1)
+					PrintResult(i);
+
 			}
-			case 4: {
-				return;
-			}
-			default: {
-				Wrong();
-				continue;
-			}
+			break;
+		}
+		case 4: {
+			return;
+		}
+		default: {
+			Wrong();
+			continue;
+		}
 		}
 		return;
 	}
 }
-
 void Print() {
 	while (1) {
-		printf("출력할 정보의 번호를 입력하세요. \n(0 입력시 취소)\n (입력된 정보의 갯수 : %d)\n입력:",i-1);
+		printf("출력할 정보의 번호를 입력하세요. \n(0 입력시 취소)\n (입력된 정보의 갯수 :%d)\n입력:", i - 1);
+
 		scanf("%d", &tempnum);
 		if (tempnum < i && tempnum>0) {
 			getchar();
@@ -204,37 +208,35 @@ void Print() {
 			continue;
 		}
 	}
-
 }
-
-int main() {
-	
-	while (1){
+int PrintResult(int num) {
+	printf("%d번 정보 :\n이름 : %s\n번호 : %d\n점수 : %d\n", num, manage[num].name, manage[num].num, manage[num].score);
+	return 0;
+}
+void Start() {
+	while (1) {
 		temp1 = Begin();
 		switch (temp1) {
-			case 1: {
-				Input(manage);
-				break;
-			}
-			case 2: {
-				Fix();
-				break;
-			}
-			case 3: {
-				Search();
-				break;
-			}
-			case 4: {
-				Print();
-				break;
-			}
-			case 5: {
-				return;
-			}
-			default: {
-				Wrong();
-				continue;
-			}
+		case 1: {
+			Input(manage);
+			break;
+
+		}
+		case 2: {
+			Fix();
+			break; }
+		case 3: {
+			Search();
+			break; }
+		case 4: {
+			Print();
+			break; }
+		case 5: {
+			return; }
+		default: {
+			Wrong();
+			continue; }
 		}
 	}
 }
+int main() { Start(); }
